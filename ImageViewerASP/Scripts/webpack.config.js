b8cc56bae110a1c150c279f6041db399c63cf400
Ichisada Shioko <yokobang99@gmail.com>
2019-10-19 20:11:23 +0700

    move npm inside Scripts directory

--- ImageViewerASP/Scripts/webpack.config.js
+++ ImageViewerASP/Scripts/webpack.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-    mode: "development",
+    mode: 'development',
     watch: true,
 
+    entry: {
+        index: './src/Index.tsx',
+        reader: './src/Reader.tsx',
+        app: './src/App.tsx',
+    },
+
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: 'source-map',
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx"]
+        extensions: ['.ts', '.tsx']
     },
 
     module: {
@@ -17,15 +23,15 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: "ts-loader"
+                        loader: 'ts-loader'
                     }
                 ]
             },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
-                enforce: "pre",
+                enforce: 'pre',
                 test: /\.js$/,
-                loader: "source-map-loader"
+                loader: 'source-map-loader'
             }
         ]
     },
@@ -35,7 +41,7 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+        'react': 'React',
+        'react-dom': 'ReactDOM'
     }
 };
